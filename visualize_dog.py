4f7e7724879761e25b3087a7bba921234806ecee
Polopopy <ray.x.han@gmail.com>
2025-09-08 01:15:19 -0400

    good standing position

--- visualize_dog.py
+++ visualize_dog.py
@@ -3,19 +3,18 @@ import mujoco as mj
 import numpy as np
 from mujoco import viewer
 
+
 def dog_walk():
     model = mj.MjModel.from_xml_path("xml/dog.xml")
     data = mj.MjData(model)
     
     t = 0.0
     dt = model.opt.timestep
+
+    # set_standing_pose(model, data)
     
     with viewer.launch_passive(model, data) as v:
         while v.is_running():
-            # data.ctrl[2] = 10 * np.sin(t)
-            # data.ctrl[6] = 10 * np.sin(t)
-            # data.ctrl[10] = 10 * np.sin(t)
-            
             
             mj.mj_step(model, data)
             v.sync()
